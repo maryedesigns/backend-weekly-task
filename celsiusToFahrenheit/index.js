d72bb6b5d92388fa88bc2df660f8afa0ffe83114
maryedesigns <mary.erhieyovwe4christ@gmail.com>
2021-06-13 16:29:00 +0100

    my second commit

---
+++ celsiusToFahrenheit/index.js
@@ -0,0 +1,23 @@
+//This function takes in one number type as its parameter
+//The function converts convert from Celsius to Fahrenheit.
+//The formular to convert to fahrenheit is "temperature in Celsius times 9 / 5, plus 32".
+
+function convertToFahrenheit(celsius) {
+    let fahrenheit;
+    return fahrenheit;
+}
+
+fahrenheit = -30 * 9 / 5 + 32;
+alert('The fahrenheit equavalent of -30 is ' + fahrenheit);
+
+fahrenheit = -10 * 9 / 5 + 32;
+alert('The fahrenheit equavalent of -10 is ' + fahrenheit);
+
+fahrenheit = 0 * 9 / 5 + 32;
+alert('The fahrenheit equavalent of 0 is ' + fahrenheit);
+
+
+
+
+convertToF(-30) // -> -22
+convertToF(-10) // -> 14
